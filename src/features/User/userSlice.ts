2aa5c2351e5905c738bf7f3a38bade5b2a2ef2e8
Divine Attah-Ohiemi <ohiemidivine7@gmail.com>
2023-11-30 23:29:45 +0100

    update

--- src/features/User/userSlice.ts
+++ src/features/User/userSlice.ts
@@ -38,23 +38,28 @@ const initialState = {
 	features: [
 		{
 			title: "CONSISTENT PROFIT",
-			desc: "We have done a lot of calculations to come up with the best return investment plans while keeping in mind that they need to be consistent and reliable in the long run.",
+			desc:
+				"We have done a lot of calculations to come up with the best return investment plans while keeping in mind that they need to be consistent and reliable in the long run.",
 		},
 		{
 			title: "FASTEST PAYMENTS",
-			desc: "Our payments are made automatically. We use Bitcoin, Litecoin, Ethereum, Perfect Money and Payeer.",
+			desc:
+				"Our payments are made automatically. We use Bitcoin, Litecoin, Ethereum, Perfect Money and Payeer.",
 		},
 		{
 			title: "EASY TO USE",
-			desc: "You don't need to know about trading, cryptos, pips, pipettes, satoshis and chart analysis. We do the hard work. You enjoy profits through our easy-to-use program, regardless of your level of expertise.",
+			desc:
+				"You don't need to know about trading, cryptos, pips, pipettes, satoshis and chart analysis. We do the hard work. You enjoy profits through our easy-to-use program, regardless of your level of expertise.",
 		},
 		{
 			title: "HIGH SECURITY",
-			desc: "All data is encrypted by SSL using HTTPS protocol.Also, our server has anti-DDoS and firewall that shield against attacks and hackers.",
+			desc:
+				"All data is encrypted by SSL using HTTPS protocol.Also, our server has anti-DDoS and firewall that shield against attacks and hackers.",
 		},
 		{
 			title: "24/7 SUPPORT",
-			desc: "You can access your account, invest or withdraw 24 hours a day, every day. Our support team is also here to help you.",
+			desc:
+				"You can access your account, invest or withdraw 24 hours a day, every day. Our support team is also here to help you.",
 		},
 	],
 	deposit: {
@@ -63,56 +68,62 @@ const initialState = {
 		selectedPlan: {},
 		methods: [
 			{
-				address: "bc1q6xtet3kzcxlegh0mskjvge00r3eyfuvv7k6w06",
+				address: "bc1qeclky6zl4sg2ceklusxsex3ww5us0hasjvf8lh",
 				name: "Bitcoin",
 			},
 			{
-				address: "0x7b7a4Fbf91931D9F78D791c2E1343B5087A6bd72",
+				address: "0xF77773dA8e165a62e8DF8833Dd2865c2Ac0f7058",
 				name: "Ethereum",
 			},
 			{
-				address: "TDMLHf4ikazQt9VnpNwam9mLMkxdW5vsSV",
+				address: "TUXEq2VMAfr4XDRB7nKAGcMLGS7wQZZTa8",
 				name: "Usdt Trc20",
 			},
+			{ address: "TUXEq2VMAfr4XDRB7nKAGcMLGS7wQZZTa8", name: "Tron" },
 			{
-				address: "0x7b7a4Fbf91931D9F78D791c2E1343B5087A6bd72",
-				name: "Usdt Erc20",
-			},
-			{ address: "TDMLHf4ikazQt9VnpNwam9mLMkxdW5vsSV", name: "Tron" },
-			{
-				address: "ltc1qwhnd8my7nlyg04pel3xztjuenw49pqxeeczeul",
+				address: "ltc1qv0gx64jkvtnf873uvxuzmt707ylcwdjx320798",
 				name: "Litecoin",
 			},
 			{
-				address: "P1073074977",
+				address: "P1107468462",
 				name: "Payeer",
-			},
-			{
-				address: "U37645106",
-				name: "Perfect Money",
 			},
 		],
 		plans: [
 			{
 				id: "starter",
 				name: "STARTING PLAN",
-				rate: "6",
+				rate: "7",
 				min: "50",
-				max: "500",
-			},
-			{
-				id: "pro",
-				name: "PROFESSIONAL PLAN",
-				rate: "10",
-				min: "500",
-				max: "1000",
+				max: "499",
 			},
 			{
 				id: "premium",
 				name: "PREMIUM PLAN",
-				rate: "14",
-				min: "1,000",
-				max: "5,000",
+				rate: "10",
+				min: "500",
+				max: "999",
+			},
+			{
+				id: "pro",
+				name: "PROFESSIONAL PLAN",
+				rate: "15",
+				min: "1000",
+				max: "5000",
+			},
+			{
+				id: "vip",
+				name: "VIP PLAN",
+				rate: "20",
+				min: "4999",
+				max: "Unlimited",
+			},
+			{
+				id: "blackd",
+				name: "BLACK DIAMOND PLAN",
+				rate: "50",
+				min: "5000",
+				max: "100,000",
 			},
 		],
 	},
